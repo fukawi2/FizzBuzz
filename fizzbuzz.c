2c588ccdd1432ea9fb07c69362cc8a1bebe5170e
Phillip Smith <phillip.smith@naturesorganics.com.au>
2018-12-26 18:09:47 +1100

    initial commit of fizzbuzz.c (very inefficient Im sure)

---
+++ fizzbuzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int n;
+int match = 0;
+
+int main() {
+
+  for ( n=1; n<=100; n++ ) {
+    if ( n % 15 == 0 ) {
+      printf("FizzBuzz\n");
+    } else if ( n % 3 == 0 ) {
+      printf("Fizz\n");
+    } else if ( n % 5 == 0 ) {
+      printf("Buzz\n");
+    } else {
+      printf("%d\n", n);
+    }
+  }
+}
